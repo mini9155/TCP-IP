f0325b9cf6457632e810cc13021e03c9bfd8941b
mini <hanbv@naver.com>
2023-06-22 17:17:43 +0900

    0622

--- Day04/thread1.c
+++ Day04/thread1.c
@@ -12,3 +12,32 @@
 스레드는 개별적으로 할당이 된다
 데이터, 힙 영역은 공통적으로 사용
 */
+
+#include <stdio.h>
+#include <pthread.h>
+void* thread_main(void *arg);
+
+int main(int argc, char *argv[]) 
+{
+	pthread_t t_id;
+	int thread_param=5;
+	
+	if(pthread_create(&t_id, NULL, thread_main, (void*)&thread_param)!=0)
+	{
+		puts("pthread_create() error");
+		return -1;
+	}; 	
+	sleep(10);  puts("end of main");
+	return 0;
+}
+
+void* thread_main(void *arg) 
+{
+	int i;
+	int cnt=*((int*)arg);
+	for(i=0; i<cnt; i++)
+	{
+		sleep(1);  puts("running thread");	 
+	}
+	return NULL;
+}
